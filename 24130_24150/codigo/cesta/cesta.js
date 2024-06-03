8fa1994c44d490507b265590804b4798bd878b56
GuilhermeProfeta <162441842+GuilhermeProfeta@users.noreply.github.com>
2024-06-03 10:06:24 -0300

    comentei o codigo

--- 24130_24150/codigo/cesta/cesta.js
+++ 24130_24150/codigo/cesta/cesta.js
@@ -1,15 +1,15 @@
 
-var produtos = [
-    {nome: "Banana", unidade: "cachos", precoPorUnidade: 1.50, classificacao: "fruta"},
-    {nome: "Brócolis", unidade: "kg", precoPorUnidade: 16.82, classificacao: "verdura"},
-    {nome: "Cenoura", unidade: "kg", precoPorUnidade: 3.99, classificacao: "legume"},
-    {nome: "Alface", unidade: "unidade", precoPorUnidade: 2.25, classificacao: "verdura"},
-    {nome: "Tomate", unidade: "kg", precoPorUnidade: 5.75, classificacao: "fruta"},
-    {nome: "Batata", unidade: "kg", precoPorUnidade: 2.50, classificacao: "legume"},
-    {nome: "Pepino", unidade: "kg", precoPorUnidade: 4.20, classificacao: "fruta"},
-    {nome: "Abobrinha", unidade: "kg", precoPorUnidade: 6.30, classificacao: "legume"},
-    {nome: "Espinafre", unidade: "kg", precoPorUnidade: 9.10, classificacao: "verdura"}
-]
+// var produtos = [
+//     {nome: "Banana", unidade: "cachos", precoPorUnidade: 1.50, classificacao: "fruta"},
+//     {nome: "Brócolis", unidade: "kg", precoPorUnidade: 16.82, classificacao: "verdura"},
+//     {nome: "Cenoura", unidade: "kg", precoPorUnidade: 3.99, classificacao: "legume"},
+//     {nome: "Alface", unidade: "unidade", precoPorUnidade: 2.25, classificacao: "verdura"},
+//     {nome: "Tomate", unidade: "kg", precoPorUnidade: 5.75, classificacao: "fruta"},
+//     {nome: "Batata", unidade: "kg", precoPorUnidade: 2.50, classificacao: "legume"},
+//     {nome: "Pepino", unidade: "kg", precoPorUnidade: 4.20, classificacao: "fruta"},
+//     {nome: "Abobrinha", unidade: "kg", precoPorUnidade: 6.30, classificacao: "legume"},
+//     {nome: "Espinafre", unidade: "kg", precoPorUnidade: 9.10, classificacao: "verdura"}
+// ]
 
 function obtemPreco(nomeProd){
     let quantidade = parseInt(localStorage.getItem(nomeProd));
